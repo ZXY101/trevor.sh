bb9586d8d9df23bb6264cbaad87dea5829cbb3e4
Shaun Tenner <39561296+ZXY101@users.noreply.github.com>
2019-05-20 09:53:35 +0200

    Added trevor

---
+++ Voyage/trevor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp .vimrc ~/
+cp .gitignore ~/
+cp -R .vim ~/ 
+git config --global core.excludesfile ~/.gitignore
+osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'
+defaults write com.apple.dock static-only -bool true; killall Dock
